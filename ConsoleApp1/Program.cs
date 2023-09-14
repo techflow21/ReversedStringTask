a5e101659a53da09e5e25e3c4791fbdcb254109f
Bello Soliu <bellosoliu12@gmail.com>
2023-09-14 12:06:04 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string statement = "Hello World!";
+            string finalResult = ReversedMethod(statement);
+            Console.WriteLine(finalResult);
+        }
+
+        public static string ReversedMethod(string statement)
+        {
+            string[] words = statement.Split(' ');
+            Array.Reverse(words);
+            return string.Join(' ', words);
+        }
+    }
+}
